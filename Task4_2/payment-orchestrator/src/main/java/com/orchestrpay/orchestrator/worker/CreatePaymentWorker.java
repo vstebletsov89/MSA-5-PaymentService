cdd8ff6604bb91e043d439f064dafd5f2d92f34f
vstebletsov89 <stebletsov-vs@yandex.ru>
2026-04-11 20:42:18 +0200

    wip

--- Task4_2/payment-orchestrator/src/main/java/com/orchestrpay/orchestrator/worker/CreatePaymentWorker.java
+++ Task4_2/payment-orchestrator/src/main/java/com/orchestrpay/orchestrator/worker/CreatePaymentWorker.java
@@ -2,6 +2,7 @@ package com.orchestrpay.orchestrator.worker;
 
 import io.camunda.zeebe.client.api.response.ActivatedJob;
 import io.camunda.zeebe.spring.client.annotation.JobWorker;
+import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -13,6 +14,11 @@ import java.util.UUID;
 public class CreatePaymentWorker {
 
     private static final Logger log = LoggerFactory.getLogger(CreatePaymentWorker.class);
+
+    @PostConstruct
+    public void init() {
+        log.info("[CREATE_PAYMENT] Worker bean initialized");
+    }
 
     @JobWorker(type = "create-payment", autoComplete = true)
     public Map<String, Object> handle(ActivatedJob job) {
